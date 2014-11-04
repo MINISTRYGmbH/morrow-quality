f6a6437eeba491898ae898d52b77d4999449c297
Christoph Erdmann <noreply@users.noreply.github.com>
2014-11-04 17:58:42 +0100

    Some path changes

--- index.php
+++ index.php
@@ -10,26 +10,28 @@
 
 ob_start();
 
+$path_to_src = realpath(__DIR__ . '/../morrow/docs/vendor/morrow/core/src');
+
 // init Codesniffer
 echo "Running CodeSniffer\n-------------------\n";
 echo shell_exec('vendor/bin/phpcs --version');
-echo shell_exec("vendor/bin/phpcs --standard=".__DIR__."/Ministry ../morrow-framework/src 2>&1");
+echo shell_exec("vendor/bin/phpcs --standard=".__DIR__."/Ministry {$path_to_src} 2>&1");
 
 // init Mess Detector
 echo "\nRunning Mess Detector\n-------------------\n";
-//echo "\nNaming Rules\n-------------------";
-//echo shell_exec("vendor/bin/phpmd ../trunk/main/vendor/Morrow text naming 2>&1");
-echo "\nUnused Rules\n-------------------";
-echo shell_exec("vendor/bin/phpmd ../morrow-framework/src text unusedcode 2>&1");
-echo "\nCodesize Rules\n-------------------";
-echo shell_exec("vendor/bin/phpmd ../morrow-framework/src text codesize 2>&1");
-echo "\nDesign Rules\n-------------------";
-echo shell_exec("vendor/bin/phpmd ../morrow-framework/src text design 2>&1");
+// echo "\nNaming Rules\n-------------------";
+// echo shell_exec("vendor/bin/phpmd ../trunk/main/vendor/Morrow text naming 2>&1");
+// echo "\nUnused Rules\n-------------------";
+// echo shell_exec("vendor/bin/phpmd {$path_to_src} text unusedcode 2>&1");
+// echo "\nCodesize Rules\n-------------------";
+// echo shell_exec("vendor/bin/phpmd {$path_to_src} text codesize 2>&1");
+// echo "\nDesign Rules\n-------------------";
+// echo shell_exec("vendor/bin/phpmd {$path_to_src} text design 2>&1");
 
 echo 'Done: ' . date('Y-m-d H:i:s');
 
 $content = ob_get_clean();
-$content = str_replace(realpath('../morrow-framework/src') . '/', '', $content);
+$content = str_replace($path_to_src, '', $content);
 echo $content;
 ?>
 </pre>
